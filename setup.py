09916bd1efd23292c559701477caab29eaf656ea
Christopher Brown <io@henrian.com>
2012-12-07 20:10:03 -0600

    Fix python setup json ref

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = path.abspath(path.dirname(__file__))
 package = json.load(open(path.join(here, 'package.json')))
 
 setup(
-    name=package['lexicons'],
+    name=package['name'],
     version=package['version'],
     author='Christopher Brown',
     author_email='chrisbrown@utexas.edu',
